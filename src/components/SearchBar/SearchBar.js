df7de3cb6519f18a6c825bc37c16aef0c8421a0d
Abdrhmaan <xaan087@gmail.com>
2023-09-14 12:24:05 -0700

    waa dhameeyy

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,19 +4,45 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faHeart, faCircle, faCompass } from '@fortawesome/free-regular-svg-icons';
 import "./SearchBar.css";
+import { useState } from "react";
+import dummyData from "../../dummy-data";
 
 const SearchBar = (props) => {
+  const [query , setquery] = useState("")
+  const [filterer , setfilter] = useState(dummyData)
+
+  console.log(props)
+
+
+
+
+
+  function handlchange(event){
+  
+    event.preventDefault()
+  
+    const filter =    dummyData.filter((item) => item.username.toLocaleLowerCase().includes(query.toLocaleLowerCase() ||
+    item.comments.some(comnte  => comnte.username.toLocaleLowerCase().includes(query.toLocaleLowerCase()))
+     
+    ))
+    setfilter(filter)
+    
+  
+    
+
+
+  }
   return (
     <div className="search-bar-wrapper">
       <div className="social">
         <FontAwesomeIcon icon={faInstagram} />
       </div>
-      <form className="search-form">
+      <form className="search-form" onChange={handlchange}>
         <input
           type="text"
           placeholder="Search"
         />
-      </form>
+      </form >
       <div className="social-wrapper">
         <div className="social">
           <FontAwesomeIcon icon={faCompass} />
